fdf610477a7e7db421360e6bad1314cebc06b2bd
diveshupadhyay-code <diveshupadhyay4@gmail.com>
2026-03-10 09:02:52 +0530

    Add environment variable for TensorFlow legacy Keras
    Set environment variable for legacy Keras compatibility.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ from PIL import Image, ImageOps
 import numpy as np
 import gdown
 import os
+os.environ['TF_USE_LEGACY_KERAS'] = '1' # Purane model ko naye system mein chalane ka jugaad
+import tensorflow as tf
 
 # --- 1. Model Loading Ka Jugad ---
 file_id = '1iKNJyPhAKIRrzR7ihhNgxrgzgSPQS-8y'
